6febd4e3a841edcc03ce7b05f6a49dc2996cc461
sourov6580 <sourov6580@gmail.com>
2026-09-11 19:48:04 +0600

    Add files via upload

--- content/site.js
+++ content/site.js
@@ -133,7 +133,7 @@ export const portfolio = {
     { t: "আমাদের ঐতিহ্য", cat: "Other Website", biz: "Organization", url: "https://amader-proshchitra.lovable.app/", image: "/images/amader-oitijjo.png" },
   ],
   video: [
-    { t: "Trendy Abaya set", cat: "Clothing & Fashion Ads", biz: "Clothing", url: "https://drive.google.com/file/d/1S_-J_TFUoXcnR6B6FIzSdWo6yXZhqxTb/view?usp=sharing", vertical: true, image: null },
+    { t: "Trendy Abaya set", cat: "Clothing & Fashion Ads", biz: "Clothing", url: "https://www.youtube.com/shorts/6XKwISjDni4", vertical: true, image: "https://img.youtube.com/vi/6XKwISjDni4/hqdefault.jpg" },
     { t: "Premium quality borka set", cat: "Clothing & Fashion Ads", biz: "Clothing", url: "https://www.youtube.com/shorts/Bw7mAAnpeVM", vertical: true, image: "https://img.youtube.com/vi/Bw7mAAnpeVM/hqdefault.jpg" },
     { t: "AI-পাওয়ার্ড ভিডিও মেকিং সার্ভিস!", cat: "Business & Service Ads", biz: "সার্ভিস", url: "https://youtube.com/shorts/ft3G1rOhk9w", vertical: true, image: "https://img.youtube.com/vi/ft3G1rOhk9w/hqdefault.jpg" },
     { t: "Intelligent Cupping Instrument", cat: "E-commerce & Product Ads", biz: "gadget", url: "https://youtube.com/shorts/nJkzRVPVOIY", vertical: true, image: "https://img.youtube.com/vi/nJkzRVPVOIY/hqdefault.jpg" },
